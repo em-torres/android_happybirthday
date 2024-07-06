acfc7e8885a37170e1c85720397bd88e328ac472
Edwin Marte Torres <kdgpro15@gmail.com>
2024-07-05 22:52:33 -0400

    feature: Creating TaskManager exercise

--- app/src/main/java/com/cuanticosoluciones/happybirthday/MainActivity.kt
+++ app/src/main/java/com/cuanticosoluciones/happybirthday/MainActivity.kt
@@ -11,11 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,62 +27,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Top) {
-                    ImageHead()
-                    ArticleContent(
-                        title = stringResource(R.string.article_title),
-                        content = stringResource(R.string.article_body),
-                        extra = stringResource(R.string.article_extra)
-                    )
-                }
+                TaskManager(Modifier)
             }
         }
     }
 }
 
-@Composable
-fun ArticleContent(modifier: Modifier = Modifier, title: String, content: String, extra: String) {
-    Column(modifier = modifier) {
-        Text(
-            text = title,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(
-            text = content,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(
-            text = extra,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun ImageHead() {
-    Image(
-        painter = painterResource(id = R.drawable.bg_compose_background),
-        contentDescription = stringResource(id = R.string.app_name),
-        contentScale = ContentScale.FillWidth
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun ArticlesPreview() {
+fun TaskManagerPreview() {
+    TaskManager(Modifier)
+}
+
+@Composable
+fun TaskManager(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top) {
-        ImageHead()
-        ArticleContent(
-            title = stringResource(R.string.article_title),
-            content = stringResource(R.string.article_body),
-            extra = stringResource(R.string.article_extra)
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = stringResource(id = R.string.tasks_completed_title),
+            contentScale = ContentScale.Crop,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.tasks_completed_title),
+            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+            modifier = modifier.padding(top=24.dp, bottom=8.dp)
+        )
+        Text(text = stringResource(id = R.string.tasks_completed_qualification),
+            fontSize = 16.sp,
+            modifier = modifier.padding(16.dp)
         )
     }
 }
