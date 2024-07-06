52e930cc489b954a0580dcad31ac8ce715119108
Edwin Marte Torres <kdgpro15@gmail.com>
2024-07-05 21:53:20 -0400

    feature: Extracting the Strings and creating a Background Image refilled

--- app/src/main/java/com/cuanticosoluciones/happybirthday/MainActivity.kt
+++ app/src/main/java/com/cuanticosoluciones/happybirthday/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,10 +35,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    GreetingImage(
                         message = "Happy Birthday Sam!",
-                        from = "From: Emma",
-                        modifier = Modifier.padding(8.dp)
+                        from = "From: Emma"
                     )
                 }
             }
@@ -45,7 +49,7 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(modifier: Modifier = Modifier, message: String = "Happy Birthday!!", from: String) {
     Column (
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp)
+        modifier = modifier
     ) {
         Text(
             text = message,
@@ -57,8 +61,8 @@ fun GreetingText(modifier: Modifier = Modifier, message: String = "Happy Birthda
             text = from,
             fontSize = 36.sp,
             modifier = Modifier
-                        .padding(16.dp)
-                        .align(alignment = Alignment.End)
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -67,6 +71,30 @@ fun GreetingText(modifier: Modifier = Modifier, message: String = "Happy Birthda
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From: Emma")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier, message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            modifier = modifier.fillMaxSize(),
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            from = from,
+            message = message,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
